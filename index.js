b8b0bf388bc5b366d3021ffebc44e6c0ae30f15c
umanrai <umanrai58@gmail.com>
2022-07-21 11:34:26 +0545

    initial javaScript learning

---
+++ index.js
@@ -0,0 +1,44 @@
+// My first JavaScript code.
+console.log('Hello World');
+
+//variable.
+let FirstName = 'Uman';
+let LastName = 'Rai.';
+console.log(FirstName , LastName);
+
+//constants
+const interestRate = 1;
+console.log(interestRate);
+
+//object
+let person={
+	name : 'uman',
+	age : 23
+}
+console.log(person);
+// dot notation Dynamic typing
+person.name = 'rai';
+console.log(person.name);
+// bracket notation Dynamic typing
+person['name'] = 'dungbocha';
+console.log(person['name']);
+
+//array
+let bikes = [ 'beneli', 'duke' ];
+bikes[2] = 'fz';
+console.log(bikes);
+
+//function
+function fun() {
+	console.log('Hello World');
+}
+fun();
+function greet(name , lastName) {
+	console.log('Hello'  + name + ' ' + lastName);
+}
+greet('Uman' , 'Rai');
+
+function square(number) {
+	return number * number;
+}
+console.log(square(4));
