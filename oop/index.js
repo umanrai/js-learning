e937007fc8b4f0b0add2beca8836b04003a396e2
umanrai <umanrai58@gmail.com>
2022-07-21 23:46:44 +0545

    add eg of encapsulation

--- oop/index.js
+++ oop/index.js
@@ -9,3 +9,12 @@ let employee={
 };
 employee.getWage();
 console.log(employee);
+
+let baseSalary = 30_000;
+let	overtime = 10;
+let	rate = 20;
+
+function getWage(baseSalary, overtime, rate){
+	return baseSalary + (overtime * rate);
+}
+console.log(getWage);
